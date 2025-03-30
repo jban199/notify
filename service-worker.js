342697cfc4b290a29d7f0b6f82c8929e7eef3f1c
jban199 <jeetbanerjee.ban@gmail.com>
2025-03-30 07:53:35 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,19 @@
-// service-worker.js
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open("pwa-cache").then(cache => {
+        caches.open('pwa-cache').then(cache => {
             return cache.addAll([
-                "index.html",
-                "styles.css",
-                "app.js",
-                "manifest.json",
-                "icon-192x192.png",
-                "icon-512x512.png"
+                '/',
+                '/index.html',
+                '/styles.css',
+                '/app.js',
+                '/manifest.json',
+                '/default-icon.png'
             ]);
         })
     );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request).then(response => {
             return response || fetch(event.request);
